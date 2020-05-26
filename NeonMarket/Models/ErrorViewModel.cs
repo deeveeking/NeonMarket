58461a66840acb235747bb08636be733303be954
deevee <thisformyworld@gmail.com>
2020-05-26 14:49:32 +0300

    First

---
+++ NeonMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NeonMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
