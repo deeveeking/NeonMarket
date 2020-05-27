4b7126b69a878c24fcb7ce84a2e9a023c8db4247
deevee <thisformyworld@gmail.com>
2020-05-27 18:27:06 +0300

    Login and Registation page

--- NeonMarket/Controllers/HomeController.cs
+++ NeonMarket/Controllers/HomeController.cs
@@ -33,5 +33,14 @@ namespace NeonMarket.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    public IActionResult Registration()
+        {
+            return View();
+        }
     }
 }
